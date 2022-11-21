3c1d8e0452e51277cd2328927c319bc8a370184a
Jan Czizikow <jan.czizikow@gmail.com>
2022-11-21 22:46:58 +0100

    repl

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+	"os/user"
 
+	"github.com/janczizikow/monkey/repl"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s!\n", user.Username)
+	fmt.Println("Type in commands")
+	repl.Start(os.Stdin, os.Stdout)
 }
