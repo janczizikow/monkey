e4f4a0cf5a0d14aa520ed3d2556fe962c4c19ec3
Jan Czizikow <jan.czizikow@gmail.com>
2022-11-21 20:38:31 +0100

    parse identifiers and numbers

--- token/token.go
+++ token/token.go
@@ -29,3 +29,16 @@ const (
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
 )
+
+var keyword = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keyword[ident]; ok {
+		return tok
+	}
+
+	return IDENT
+}
